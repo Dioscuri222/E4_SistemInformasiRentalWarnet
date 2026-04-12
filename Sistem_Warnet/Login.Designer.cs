f5395f42a4165d5b05b766be5d9fd0352d334046
Fasya Tri.N <fasyanovember@gmail.com>
2026-04-12 21:28:31 +0700

    membuat textbox login

--- Sistem_Warnet/Login.Designer.cs
+++ Sistem_Warnet/Login.Designer.cs
@@ -32,6 +32,8 @@
             this.btnLogou = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnLogin
@@ -71,11 +73,27 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Password";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(263, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(307, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(263, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(307, 22);
+            this.textBox2.TabIndex = 6;
+            // 
             // Login_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnLogou);
@@ -94,6 +112,8 @@
         private System.Windows.Forms.Button btnLogou;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
