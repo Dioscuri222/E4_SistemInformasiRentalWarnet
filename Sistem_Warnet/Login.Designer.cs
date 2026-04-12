2a2ed095196d2312024128a0bc4d01fc700d7632
Fasya Tri.N <fasyanovember@gmail.com>
2026-04-12 21:27:56 +0700

    membuat label untuk login

--- Sistem_Warnet/Login.Designer.cs
+++ Sistem_Warnet/Login.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.btnLogin = new System.Windows.Forms.Button();
             this.btnLogou = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnLogin
@@ -50,17 +52,39 @@
             this.btnLogou.Text = "Logout";
             this.btnLogou.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
             // Login_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnLogou);
             this.Controls.Add(this.btnLogin);
             this.Name = "Login_Form";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +92,8 @@
 
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Button btnLogou;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Sistem_Warnet/Login.cs
+++ Sistem_Warnet/Login.cs
@@ -21,5 +21,10 @@ namespace Sistem_Warnet
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
