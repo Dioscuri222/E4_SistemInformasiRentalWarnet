ec1e766b4e883f8f98de17b895fa6e7fadf894d3
Fasya Tri.N <fasyanovember@gmail.com>
2026-04-14 21:52:00 +0700

    membuat koneksi

--- Sistem_Warnet/WarnetPage.cs
+++ Sistem_Warnet/WarnetPage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace Sistem_Warnet
 {
     public partial class Warnet_Form : Form
     {
+        private string connectionString = "Data Source=FASYALTP\\FASYALTP;Initial Catalog=DBWarnet;Integrated Security=True";
+        private SqlConnection connection;
         public Warnet_Form()
         {
             InitializeComponent();
