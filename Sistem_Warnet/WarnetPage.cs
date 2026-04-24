d473dfb418356967dbb7c788e32d4577342eef8a
Fasya Tri.N <fasyanovember@gmail.com>
2026-04-24 18:58:31 +0700

    Revisi: Menambah class Staff

--- Sistem_Warnet/WarnetPage.cs
+++ Sistem_Warnet/WarnetPage.cs
@@ -21,6 +21,13 @@ namespace Sistem_Warnet
             conn = new SqlConnection(connectionString);
         }
 
+        public class Staff
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Role { get; set; }
+        }
+
         private void LoadData()
         {
             try
