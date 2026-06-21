b04449f9a048accb496f0918e765c8d49ff77a62
zakibn18 <jekben815@gmail.com>
2026-06-21 15:24:59 +0700

    ubah SP transaction

--- SPTransaction.sql
+++ SPTransaction.sql
@@ -1,70 +1,60 @@
--- 1. SP untuk Insert Transaksi (Mengembalikan ID baru dengan OUTPUT INSERTED)
-CREATE PROCEDURE sp_InsertTransaksi
+CREATE PROCEDURE sp_ProsesTransaksiKasirLengkap
     @id_user INT,
     @id_tier INT,
-    @durasi_jam INT,
-    @total_bayar INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-    INSERT INTO Transaksi_Pembelian (id_user, id_tier, durasi_jam, total_bayar) 
-    OUTPUT INSERTED.id_transaksi 
-    VALUES (@id_user, @id_tier, @durasi_jam, @total_bayar);
-END
-GO
-
--- 2. SP untuk Insert Voucher
-CREATE PROCEDURE sp_InsertVoucher
-    @kode_voucher VARCHAR(20),
-    @id_transaksi INT,
     @id_pc INT,
-    @sisa_waktu_menit INT
+    @durasi_jam INT,
+    @total_bayar INT,
+    @kode_voucher VARCHAR(20)
 AS
 BEGIN
+    -- Matikan pesan ' (1 row affected)' agar performa jaringan jauh lebih cepat
     SET NOCOUNT ON;
-    INSERT INTO Voucher_Sesi (kode_voucher, id_transaksi, id_pc, sisa_waktu_menit, status_sesi) 
-    VALUES (@kode_voucher, @id_transaksi, @id_pc, @sisa_waktu_menit, 'Aktif');
-END
-GO
 
--- 3. SP untuk Update Status PC saat dibeli
-CREATE PROCEDURE sp_UpdateStatusPCDigunakan
-    @id_pc INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-    UPDATE Master_PC SET status = 'Digunakan' WHERE id_pc = @id_pc;
-END
-GO
+    -- MULAI BLOK PENANGKAP ERROR (T-SQL TRY...CATCH)
+    BEGIN TRY
+        -- 1. KUNCI PINTU TRANSAKSI
+        BEGIN TRANSACTION;
 
-CREATE PROCEDURE sp_GetTabelPCKasir
-AS
-BEGIN
-    SET NOCOUNT ON;
-    
-    SELECT p.id_pc AS 'ID PC', 
-           p.nomor_pc AS 'Nomor PC', 
-           t.nama_tier AS 'Kategori', 
-           t.harga_per_jam AS 'Harga/Jam', 
-           p.status AS 'Status'
-    FROM Master_PC p 
-    JOIN Tier_PC t ON p.id_tier = t.id_tier;
-END
-GO
+        -- Siapkan variabel penampung ID Transaksi yang baru lahir
+        DECLARE @new_id_transaksi INT;
 
-CREATE PROCEDURE sp_GetLaporanPendapatan
-    @TglAwal DATE,
-    @TglAkhir DATE
-AS
-BEGIN
-    SET NOCOUNT ON;
-    SELECT tp.id_transaksi AS 'ID Transaksi',
-           t.nama_tier AS 'Kategori Paket',
-           tp.durasi_jam AS 'Durasi (Jam)',
-           tp.total_bayar AS 'Pendapatan (Rp)',
-           tp.tgl_transaksi AS 'Waktu Pembelian'
-    FROM Transaksi_Pembelian tp
-    JOIN Tier_PC t ON tp.id_tier = t.id_tier
-    WHERE CAST(tp.tgl_transaksi AS DATE) BETWEEN @TglAwal AND @TglAkhir;
+        -- [PROSES A] Insert Transaksi Pembelian
+        INSERT INTO Transaksi_Pembelian (id_user, id_tier, tgl_transaksi, durasi_jam, total_bayar)
+        VALUES (@id_user, @id_tier, GETDATE(), @durasi_jam, @total_bayar);
+
+        -- SCOPE_IDENTITY() menangkap id_transaksi yang baru saja tercipta di baris atas
+        SET @new_id_transaksi = SCOPE_IDENTITY();
+
+        -- [PROSES B] Insert Voucher Sesi
+        DECLARE @sisa_menit INT = @durasi_jam * 60;
+        
+        INSERT INTO Voucher_Sesi (kode_voucher, id_transaksi, id_pc, waktu_mulai, sisa_waktu_menit, status_sesi)
+        VALUES (@kode_voucher, @new_id_transaksi, @id_pc, GETDATE(), @sisa_menit, 'Aktif');
+
+        -- [PROSES C] Update Status PC
+        UPDATE Master_PC 
+        SET status = 'Digunakan' 
+        WHERE id_pc = @id_pc;
+
+        -- JIKA SAMPAI BARIS INI TIDAK ADA YANG MELEDAK, SAHKAN PERMANEN!
+        COMMIT TRANSACTION;
+
+    END TRY
+    BEGIN CATCH
+        -- JIKA ADA ERROR DI TITIK MANAPUN (Misal: kode voucher kembar / tipe data salah)
+        -- CEK APAKAH TRANSAKSI MASIH TERBUKA
+        IF @@TRANCOUNT > 0
+        BEGIN
+            -- BATALKAN SELURUH PERUBAHAN! Uang ditarik, PC kembali Tersedia.
+            ROLLBACK TRANSACTION;
+        END
+
+        -- Tangkap pesan error asli dari SQL Server, lalu lempar ke aplikasi C#
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        DECLARE @ErrorSeverity INT = ERROR_SEVERITY();
+        DECLARE @ErrorState INT = ERROR_STATE();
+
+        RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+    END CATCH
 END
 GO
